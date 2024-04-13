f381329c922be2cd10005dd075da1bc92f5b9e78
Sebastjan Rijavec <sebastjan.rijavec@gmail.com>
2024-04-13 11:57:26 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,44 @@
+# Imports
+import ollama
+import random
+import mastodon
+from mastodon import Mastodon
+
+# Read curses file
+file = open('./haddocks.txt', 'r')
+data = file.read()
+file.close()
+
+# Format list
+datalist = data.split('\n')
+
+# Pick random curse
+randomindex = random.randint(0, len(datalist) -1)
+randomcurse = datalist[randomindex]
+
+# Remove random curse from the list
+del datalist[randomindex]
+
+# Write shortened list back
+file=open('./haddocks.txt', 'w')
+for item in datalist: 
+    file.write(item + '\n')
+file.close()
+
+# llama-2 
+# Formulate response
+response = ollama.chat(model='llama2', messages=[
+  {
+    'role': 'user',
+    'content': 'What does Captain Haddock mean when he says: {} in less than 300 characters:'.format(randomcurse),
+  },
+])
+
+# Print Results
+#print(response['message']['content'])
+
+#result = randomcurse + ':\n' + response['message']['content']
+result = randomcurse + ': ' + str(response['message']['content'])
+
+m = Mastodon(access_token="###Your#Key#here####", api_base_url="https://mastodon.social/")
+m.toot(result)
